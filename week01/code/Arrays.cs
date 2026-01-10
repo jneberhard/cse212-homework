866f03b9575554921489f6cf5e29fc47e0751b60
jneberhard <jreberhard3@gmail.com>
2026-01-10 13:23:51 -0700

    Wk1 Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,17 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Create array to store the multiples
+        double[] multiples = new double[length];
 
-        return []; // replace this return statement with your own
+        // loop from 0 to length - 1
+        for (int i = 0; i < length; i++)
+        {
+            // multiply number by the iteration number + 1 (so it doesn't multiply by zero)
+            multiples[i] = number * (i + 1);
+        }
+        // return the array of multiples
+        return multiples;
     }
 
     /// <summary>
@@ -25,7 +30,18 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
+        //How many items are in the list
+        int items = data.Count;
+
+        //determine the split point by subtracting the amount from the total number if items. Copy those items to a new list called rotated
+        List<int> rotated = data.GetRange(items - amount, amount);
+
+        //remove the last items from the list starting at the split point and going to the end of the list .
+        data.RemoveRange(items - amount, amount);
+
+        //make a new list to hold the rotated values by taking the "rotated" list and putting it at the front of the new list at index 0.
+        data.InsertRange(0, rotated);
+
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
